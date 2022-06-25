30c948e0a84ffb33316219e0dbb6700fac00b646
Raheel Khan <irahilkhan@gmail.com>
2022-06-25 22:59:44 +0500

    comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -46,7 +46,6 @@ class Comment extends Model
 
     public function getCreatedAtHumanDiffAttribute(): string{
         $created_at = $this->attributes['created_at'];
-        // return Carbon::create($created_at)->diffForHumans();
         return Carbon::create($created_at)->format('d M Y');
     }
 }
